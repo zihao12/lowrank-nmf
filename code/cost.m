b19a7c356160a3b8fb79a3de47cfc861005bbedf
zihao12 <wangzh@uchicago.edu>
2019-05-15 22:50:59 -0500

    set default of cost to add 0

--- code/cost.m
+++ code/cost.m
@@ -1,5 +1,8 @@
 % Compute the value of the objective function (the negative
 % log-likelihood under the Poisson model) up to a normalizing
 % constant.
-function f = cost (X, AB)
-  f = sum(sum(AB - X.*log(AB + eps)));
+function f = cost (X, AB, e)
+	if nargin < 3
+		e = 0;
+	end	
+  f = sum(sum(AB - X.*log(AB + e)));
